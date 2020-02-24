3039b590ac623c77c5aeb65f444b53b7ef58dd5b
Sean Doherty <sean.doherty@crosscomm.net>
2020-02-24 13:19:09 -0500

    Updating server to remove express dependency

--- server.js
+++ server.js
@@ -1,24 +1,79 @@
-let express = require('express');
-let app = express();
-let bodyParser = require('body-parser');
-let port = 8080;
-let book = require('./app/routes/book');
+const http = require('http');
+const finalHandler = require('finalhandler');
+const Router = require('router');
+const bodyParser = require('body-parser');
+const Book = require('./app/models/book')
 
-app.use(bodyParser.json());
+// Setup router
+var myRouter = Router();
+myRouter.use(bodyParser.json());
 
-app.get("/", (req, res) => res.json({message: "Welcome to our Bookstore!"}));
+// This function is a bit simpler...
+let server = http.createServer(function (request, response) {
+  	myRouter(request, response, finalHandler(request, response))
+}).listen(8080);
 
-app.route("/book")
-	.get(book.getBooks)
-	.post(book.postBook);
+// Notice how much cleaner these endpoint handlers are...
+myRouter.get('/book', function(request,response) {
+	// Return all books in the db
+	response.writeHead(200, { "Content-Type": "application/json" });
+	return response.end(JSON.stringify(Book.getAll()));
+});
 
-app.route("/book/:id")
-	.get(book.getBook)
-	.delete(book.deleteBook)
-	.put(book.updateBook);
+myRouter.post('/book', function(request,response) {// Get our query params from the query string
+	// Add book to database
+	Book.addBook(request.body)
+	
+	// Return success 
+	response.writeHead(200);
+	return response.end();
+});
 
-app.listen(port);
+myRouter.get('/book/:id', function(request,response) {// Get our query params from the query string
+	//Find book to return
+	const foundBook = Book.getBook(request.params.id)
+	// Return 404 if not found
+	if (!foundBook) {
+		response.writeHead(404);	
+		return response.end("Book Not Found");
+	}
+	// Return book information
+	response.writeHead(200, { "Content-Type": "application/json" });
+	return response.end(JSON.stringify(foundBook));
+});
 
-console.log("Listening on port " + port);
+myRouter.delete('/book/:id', function(request,response) {// Get our query params from the query string
+	// Check if book exists
+	const foundBook = Book.getBook(request.params.id)
+	
+	// Return 404 if not found
+	if (!foundBook) {
+		response.writeHead(404);	
+		return response.end("Book Not Found");
+	}
 
-module.exports = app; // for testing
+	// Remove book from book list
+	Book.remove(request.params.id);
+
+	// Return success
+	response.writeHead(200);
+	return response.end();
+});
+
+myRouter.put('/book/:id', function(request,response) {// Get our query params from the query string
+	// Check if book exists
+	const foundBook = Book.getBook(request.params.id)
+	
+	// Return 404 if not found
+	if (!foundBook) {
+		response.writeHead(404);	
+		return response.end("Book Not Found");
+	}
+
+	// Update existing book
+	const updatedBook = Book.updateBook(request.body)
+	
+	response.writeHead(200, { "Content-Type": "application/json" });
+	return response.end(JSON.stringify(updatedBook));
+});
+module.exports = server;
